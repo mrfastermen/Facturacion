e131d2fef6253a541e8cdae42de44296cd7e4fdd
Alberto Rodriguez <Alberto Rodriguez@192.168.1.8>
2014-11-01 01:41:24 -0500

    tercera version

--- src/facturacion/Main.java
+++ src/facturacion/Main.java
@@ -7,6 +7,8 @@
 package facturacion;
 
 import diseño.VentanaLogin;
+import diseño.VentanaPrincipal;
+import java.awt.Dialog;
 
 /**
  *
@@ -18,6 +20,23 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
         VentanaLogin obj = new VentanaLogin();
         obj.setVisible(true);
         obj.setLocationRelativeTo(null);
